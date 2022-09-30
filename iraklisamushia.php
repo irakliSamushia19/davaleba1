4d7e96f6dbe783d30e9d9054dbb5276ba529d6f6
irakliSamushia19 <105517113+irakliSamushia19@users.noreply.github.com>
2022-09-30 22:42:19 +0400

    Create iraklisamushia.php

---
+++ iraklisamushia.php
@@ -0,0 +1,20 @@
+<?php
+   /* Online PHP Compiler (Interpreter) and Editor */
+   do{
+        $num1 = readline("შეიყვანეთ პირველი რიცხვი \n" );
+        $num2 = readline("შეიყვანეთ მეორე რიცხვი");
+       
+        if(intval($num1) > 0 && intval($num2) < 100){
+            $x = intval($num1) * intval($num2);
+            echo $x;
+            break;
+        }
+        else if(intval($num1) == 0 || intval($num2) == 0){
+            break;
+        }
+        else{
+            echo "xelaxla scade";
+        }
+    }while ($num1 < $num2);
+    
+?>
